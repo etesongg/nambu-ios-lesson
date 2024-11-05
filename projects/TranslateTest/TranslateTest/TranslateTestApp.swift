f0877b87ebfd91bcca7522d863123864a5d6f92a
etesongg <etesong0114@gmail.com>
2024-11-05 09:49:58 +0900

    Azure AI services 결과 js로 결과 만들기

---
+++ projects/TranslateTest/TranslateTest/TranslateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateTestApp.swift
+//  TranslateTest
+//
+//  Created by 정송희 on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/TranslateTest/TranslateTest/TranslateView.swift
@@ -0,0 +1,18 @@
+//
+//  TranslateView.swift
+//  TranslateTest
+//
+//  Created by 정송희 on 11/4/24.
+//
+
+import SwiftUI
+
+struct TranslateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslateView()
+}
